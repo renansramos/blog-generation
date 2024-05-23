342d3f914162c54843edab342be6032b7ee01ada
Renan Ramos <renan_dasilvaramos@hotmail.com>
2024-05-23 16:21:51 -0300

    ATIVIDADE CRUD BACKEND BLOG-PESSOAL – PARTE 03

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,25 +20,26 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import jakarta.validation.Valid;
 
-@RestController // Anotação que diz para Spring que essa é uma controladora de rotas e acesso aos métodos 
-@RequestMapping("/postagens") //rota para chegar nessa classe "Insominia"
-@CrossOrigin(origins = "*", allowedHeaders = "*") //Liberar o acesso a outras rotas 
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PostagemController {
 	
-	@Autowired //Injeção de dependência | Instanciar a classe PostagemRepository
+	@Autowired
 	private PostagemRepository postagemRepository;
 	
-	@GetMapping //Define o método http que atende esse método 
-	public ResponseEntity<List<Postagem>> getAll(){
-		//ResponseEntity - Classe do 
-		return ResponseEntity.ok(postagemRepository.findAll());
-		//SELECT * FROM tb_postagens
-	}
+	@Autowired
+	private TemaRepository temaRepository;
 	
-	//localhost:8080/postagens/1
+	@GetMapping
+	public ResponseEntity<List<Postagem>> getAll(){
+		return ResponseEntity.ok(postagemRepository.findAll());
+	}
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Postagem> getById(@PathVariable Long id){
 		return postagemRepository.findById(id)
@@ -47,26 +47,34 @@ public class PostagemController {
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 	
-	//SELECT * FROM tb_postagens WHERE titulo = "titulo";
 	@GetMapping("/titulo/{titulo}")
 	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
 		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
 	}
-	
-	//INSERT INTO tb_postagens (titulo, texto, data) VALUES ("Título","Texto", "2024-12-31 14:05:01");
+
 	@PostMapping
 	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
-		//Retorno em formato RespondeEntity
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(postagemRepository.save(postagem));
+		if (temaRepository.existsById(postagem.getTema().getId()))
+			return ResponseEntity.status(HttpStatus.CREATED)
+					.body(postagemRepository.save(postagem));
+			
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!", null);
 	}
 	
 	@PutMapping
-	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
-		return postagemRepository.findById(postagem.getId())
-				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
-				.body(postagemRepository.save(postagem)))
-				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+		if (postagemRepository.existsById(postagem.getId())){
+			
+			if (temaRepository.existsById(postagem.getTema().getId()))
+				return ResponseEntity.status(HttpStatus.OK)
+						.body(postagemRepository.save(postagem));
+			
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!", null);
+			
+		}			
+			
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		
 	}
 	
 	@ResponseStatus(HttpStatus.NO_CONTENT)
@@ -79,21 +87,5 @@ public class PostagemController {
 		
 		postagemRepository.deleteById(id);				
 	}
+	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,7 +1,5 @@
 package com.generation.blogpessoal.repository;
 
-
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,13 +7,8 @@ import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Postagem;
 
-//JPA Repository - Classe JPA - metodos que vão realizar Query no banco 
-
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
-	//SELECT * from tb_postagens
 	
-	
-	//SELECT * FROM tb_postagens WHERE titulo LIKE "%POST%"; = findAllbyTituloContainingIgnoreCase
-	List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+    List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
     
 }
